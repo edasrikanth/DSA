d56785c9a940af33ab55bee80d05877088d5f340
Eeda Srikanth Reddy <edasrikanth1998@gmail.com>
2025-03-07 23:51:08 +0530

    factorial of n function is added

--- Recurssion/Recurssion.cpp
+++ Recurssion/Recurssion.cpp
@@ -14,12 +14,26 @@ int sum_of_n_natural_nums(int n)
 
 }
 
+int factorial_of_n(int n)
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n*factorial_of_n(n-1));
+    }
+
+}
 int main()
 {
 
     std::cout <<"Hello World!!"<<std::endl;
 
     std::cout <<"The result of first n-natural number for n = 10 is :" << sum_of_n_natural_nums(10)<<std::endl;
+
+    std::cout << "The result of factorial of n = 5 is " << factorial_of_n(5)<<std::endl;
 
     return 0;
 }

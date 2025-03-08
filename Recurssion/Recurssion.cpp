ee3e4364e261b44294386c07bb5ab1b3b63f96fb
Eeda Srikanth Reddy <edasrikanth1998@gmail.com>
2025-03-08 21:39:18 +0530

    added power, taylor_series, fibnocci_series function

--- Recurssion/Recurssion.cpp
+++ Recurssion/Recurssion.cpp
@@ -26,6 +26,96 @@ int factorial_of_n(int n)
     }
 
 }
+
+int power_of_m_n(int m, int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+       return (m*power_of_m_n(m, n-1));
+    }
+}
+
+int power_of_m_n_reducing_multiplications(int m, int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        if(n%2 == 0)
+        {
+        return (power_of_m_n_reducing_multiplications(m*m, n/2));
+        }
+
+        else
+        {
+            return (m*power_of_m_n_reducing_multiplications(m*m, (n-1)/2));
+        }
+    }
+}
+
+double taylor_series_e_pow_x_function(double x, double n)
+{
+    static double p = 1;
+    static double f = 1;
+    double r;
+    if(n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+       r = taylor_series_e_pow_x_function(x, n-1);
+       p = p*x;
+       f = f*n;
+
+       return (r + p/f);
+    }
+}
+
+int fibonacci_series(int n)
+{
+    if(n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibonacci_series(n-2) + fibonacci_series(n-1);
+    }
+}
+
+
+int fibonacci_series_red_exce_recurssion(int n)
+{
+    static int F[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+
+    if(n <= 1)
+    {
+        F[n] = n;
+        return F[n];
+    }
+    else
+    {
+        if(F[n-1] == -1)
+        {
+            F[n-1] = fibonacci_series_red_exce_recurssion(n-1);
+        }
+
+        if(F[n-2] == -1)
+        {
+            F[n-2] = fibonacci_series_red_exce_recurssion(n-2);
+        }
+
+        return (F[n-1] + F[n-2]);
+    }
+}
+
 int main()
 {
 
@@ -33,7 +123,16 @@ int main()
 
     std::cout <<"The result of first n-natural number for n = 10 is :" << sum_of_n_natural_nums(10)<<std::endl;
 
-    std::cout << "The result of factorial of n = 5 is " << factorial_of_n(5)<<std::endl;
+    std::cout << "The result of factorial of n = 5 is :" << factorial_of_n(5)<<std::endl;
 
+    std::cout << "The Result of power of function for 2^5 is :"<< power_of_m_n(2, 5)<< std::endl;
+
+    std::cout << "The Result of power of function reducing no.of multiplication for 2^5 is :"<<power_of_m_n_reducing_multiplications(2, 5)<< std::endl;
+
+    std::cout << "The Result of taylor series for e^x for 4 terms is :"<< taylor_series_e_pow_x_function(3, 4);
+
+    std::cout << "The Result of Fibonacci series of 7th term is :"<< fibonacci_series(7)<< std::endl;
+
+    std::cout << "The result of Fibonacci series of 7th term by reducing excessive recurssion is: "<<fibonacci_series_red_exce_recurssion(7)<< std::endl;
     return 0;
 }
